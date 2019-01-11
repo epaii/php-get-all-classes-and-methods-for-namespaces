76cea4bca9fd6a422ac1b0f581d534808175a3e6
MrRen <543169072@qq.com>
2019-01-11 11:00:48 +0800

    init

--- src/ClassTools.php
+++ src/ClassTools.php
@@ -21,6 +21,7 @@ class ClassTools
 
         $list = spl_autoload_functions();
         if (count($list) > 0 && ($classes = $list[0])) {
+            ob_start();
             $loader = null;
             foreach ($classes as $class) {
                 if ($class instanceof ClassLoader) {
@@ -31,13 +32,14 @@ class ClassTools
             if ($loader) {
                 $path_list = $loader->getPrefixesPsr4();
 
+
                 if (count($path_list) > 0) {
                     $namespace_keys = array_keys($path_list);
                     $dir_list = [];
 
                     foreach ($namespaces as $namespace) {
 
-                        $namespace = rtrim($namespace, "\\");
+                        $namespace = rtrim($namespace, "\\")."\\";
                         foreach ($namespace_keys as $key) {
 
                             if (stripos($namespace, $key) === 0) {
@@ -45,7 +47,7 @@ class ClassTools
                                 foreach ($path_list[$key] as $path) {
                                     $path_es[] = $path . str_replace("\\", DIRECTORY_SEPARATOR, str_replace($key, DIRECTORY_SEPARATOR, $namespace));
                                 }
-                                $dir_list[$namespace] = $path_es;
+                                $dir_list[rtrim($namespace, "\\")] = $path_es;
                             }
 
                         }
@@ -55,6 +57,8 @@ class ClassTools
                 }
 
             }
+
+            ob_end_clean();
         }
         return [];
 
@@ -62,30 +66,51 @@ class ClassTools
 
     private static function getClassesAndMethodsByPath(array $name_dir,bool $include_parent=false): array
     {
+
+
         $out = [];
         foreach ($name_dir as $name => $dirs) {
             foreach ($dirs as $dir) {
 
+
                 $stacks = [$dir];
+                $name_o = $name;
 
                 while ($directory = array_shift($stacks)) {
 
+
+                    $name_add = "";
+                    if ($dir_more = str_replace($dir,"",$directory))
+                    {
+                        $name_add = "\\".str_replace(DIRECTORY_SEPARATOR,"\\",$dir_more);
+                    }
+                    $name = $name_o.$name_add;
+
+                    $directory = rtrim($directory,DIRECTORY_SEPARATOR);
+
                     $mydir = opendir($directory);
+
 
                     while (($file = readdir($mydir))!==false) {
 
+
                         if ((is_dir("$directory/$file")) AND ($file != ".") AND ($file != "..")) {
+
                             array_push($stacks, "$directory/$file");
 
                         } else if (substr_compare($file, ".php", -4) === 0) {
                             $class = $name . "\\" . str_replace(".php", "", $file);
+
+
                             if (class_exists($class)) {
-                               $list_tmp = (new ReflectionClass($class))->getMethods();
-                               foreach ($list_tmp as $list_item)
-                               {
-                                   if ($include_parent || $list_item->class===$class)
-                                   $out[$list_item->class][] =  $list_item->name;
-                               }
+                                $list_tmp = (new ReflectionClass($class))->getMethods();
+                                foreach ($list_tmp as $list_item)
+                                {
+                                    if ($include_parent || $list_item->class===$class)
+                                        $out[$list_item->class][] =  $list_item->name;
+                                }
+                            }else{
+
                             }
 
                         }
@@ -97,6 +122,8 @@ class ClassTools
 
             }
         }
+
+
         return $out;
     }
 }
